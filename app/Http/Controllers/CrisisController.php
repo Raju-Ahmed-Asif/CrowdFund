06bbaa2c25538b063f8f04977184bcca7df6078c
zidankhan86 <zidankhan718@gmail.com>
2023-08-07 00:58:00 +0600

    validate

--- app/Http/Controllers/CrisisController.php
+++ app/Http/Controllers/CrisisController.php
@@ -27,17 +27,22 @@ class CrisisController extends Controller
     // dd($request->all());
 
 
-  $validator = Validator::make($request->all(), [
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'description' => 'required|string',
+        'image' => 'required|string', // You may add validation rules for image if needed
+        'donor' => 'required', // Adjust the validation rule for donor as needed
+        'from_date' => 'required|date|after_or_equal:today', // Validate that from_date is today or in the future
+        'to_date' => 'required|date|after_or_equal:from_date', // Validate that to_date is after or equal to from_date
+        'amount_need' => 'required|numeric|min:0',
+        'amount_raised' => 'required|numeric|min:0',
+        'about_crisis' => 'required|string',
+    ]);
 
-    'name' => 'required|string|max:100',
-    'amount_raised' => 'required|numeric|min:0',
-    'amount' => 'required|numeric|min:0',
-]);
-
-if ($validator->fails()) {
-
-    return redirect()->back()->withErrors($validator)->withInput();
-}
+    if ($validator->fails()) {
+        // If validation fails, redirect back with errors and input data
+        return redirect()->back()->withErrors($validator)->withInput();
+    }
      //dd($request->all());
 
 
